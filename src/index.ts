c31dbe58fe4601d162d335e85a891eaa28b8175c
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2024-03-27 21:21:22 +0800

    撤回上一个提交中的修改

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { Plugin, getFrontend, getBackend } from "siyuan";
 import { createApp } from "vue";
 import { onMounted } from 'vue';
 import SearchVue from "./Search.vue";
-import "./index.scss";
+import "./index.scss"
 
-const CLASS_NAME = "highlight-search-result";
+const CLASS_NAME = "highlight-search-result"
 
 const SearchComponent = {
     setup() {
@@ -22,32 +22,14 @@ const SearchComponent = {
 
 export default class PluginHighlight extends Plugin {
 
-    addSearchElement(edit) {
-        const existingElement = edit.querySelector(`.${CLASS_NAME}`);
-
-        if (!existingElement) {
-            const element = document.createElement("div");
-            element.className = CLASS_NAME;
-            edit.appendChild(element);
-            console.log(element, edit);
-
-            createApp(SearchVue, {
-                data: edit,
-                element: element,
-            }).component('search-component', SearchComponent).mount(element);
-        }
-    }
-
     onload() {
+
         this.addTopBar({
             icon: "iconMark",
             title: this.i18n.topBarTitle,
             position: "right",
             callback: () => {
-                const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container");
-                edits.forEach(edit => {
-                    this.addSearchElement(edit);
-                });
+                this.addSearchElement();
             }
         });
 
@@ -55,10 +37,7 @@ export default class PluginHighlight extends Plugin {
             langKey: "showDialog",
             hotkey: "⌥⇧⌘F",
             callback: () => {
-                const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container");
-                edits.forEach(edit => {
-                    this.addSearchElement(edit);
-                });
+                this.addSearchElement();
             },
         });
 
@@ -75,5 +54,25 @@ export default class PluginHighlight extends Plugin {
 
     uninstall() {
         console.log("uninstall");
+    }
+
+    addSearchElement() {
+        const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container");
+        console.log(edits);
+        edits.forEach(edit => {
+            const existingElement = edit.querySelector(`.${CLASS_NAME}`);
+    
+            if (!existingElement) {
+                const element = document.createElement("div");
+                element.className = CLASS_NAME;
+                edit.appendChild(element);
+                console.log(element, edit);
+    
+                createApp(SearchVue, {
+                    document: edit,
+                    element: element,
+                }).component('search-component', SearchComponent).mount(element);
+            }
+        });
     }
 }
