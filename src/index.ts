1c01d3798a37cefd863387a84c47c8dc4864b9bd
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2024-03-27 20:51:16 +0800

    不重复创建元素 #5

--- src/index.ts
+++ src/index.ts
@@ -41,30 +41,51 @@ export default class PluginHighlight extends Plugin {
         console.log("uninstall");
     }
 
-    addSearchElement() {
-        const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container")
-        console.log(edits)
-        edits.forEach(edit => {
-            let element: Element
+    // addSearchElement() {
+    //     const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container")
+    //     console.log(edits)
+    //     edits.forEach(edit => {
+    //         let element: Element
 
-            const lastElementChild = edit.lastElementChild
-            const classList = lastElementChild.classList
-            const isExist = classList.contains(CLASS_NAME)
-            console.log(isExist, lastElementChild)
+    //         const lastElementChild = edit.lastElementChild
+    //         const classList = lastElementChild.classList
+    //         const isExist = classList.contains(CLASS_NAME)
+    //         console.log(isExist, lastElementChild)
             
-            if (!isExist) {
-                element = document.createElement("div")
-                element.className = CLASS_NAME
-                edit.appendChild(element)
-                console.log(element, edit)
-            } else {
-                element = lastElementChild
-            }
+    //         if (!isExist) {
+    //             element = document.createElement("div")
+    //             element.className = CLASS_NAME
+    //             edit.appendChild(element)
+    //             console.log(element, edit)
+    //         } else {
+    //             element = lastElementChild
+    //         }
 
-            createApp(SearchVue, {
-                document: edit,
-                element: element,
-            }).mount(element)
-        })
+    //         createApp(SearchVue, {
+    //             document: edit,
+    //             element: element,
+    //         }).mount(element)
+    //     })
+    // }
+    // 在编辑区域寻找具有类名 layout-tab-container 的元素，然后检查该元素下是否存在类名为 highlight-search-result 的子元素。
+    // 如果不存在，则创建一个并将 Vue 应用挂载在新创建的元素上
+    addSearchElement() {
+        const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container");
+        console.log(edits);
+        edits.forEach(edit => {
+            const existingElement = edit.querySelector(`.${CLASS_NAME}`);
+    
+            if (!existingElement) {
+                const element = document.createElement("div");
+                element.className = CLASS_NAME;
+                edit.appendChild(element);
+                console.log(element, edit);
+    
+                createApp(SearchVue, {
+                    document: edit,
+                    element: element,
+                }).mount(element);
+            }
+        });
     }
 }
