c438e3eb53842c9c3aa7532c545f7d6395189ab2
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2024-04-17 23:28:05 +0800

    #12

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ export default class PluginHighlight extends Plugin {
     }
 
     addSearchElement() {
-        const edits = document.querySelectorAll(".layout__center [data-type='wnd'] > .layout-tab-container");
+        const edits = document.querySelectorAll(".layout__center");
         console.log(edits);
         edits.forEach(edit => {
             const existingElement = edit.querySelector(`.${CLASS_NAME}`);
