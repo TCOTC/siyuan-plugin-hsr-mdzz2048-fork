04c6be4d439c4e423b6b1e755ad0d629eb21d5b5
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2024-04-21 12:19:27 +0800

    平滑滚动

--- src/Search.vue
+++ src/Search.vue
@@ -241,14 +241,11 @@ function scroollIntoRanges(index: number) {
     // const parent = range.commonAncestorContainer.parentElement
     // parent.scrollIntoView({ behavior: 'smooth', block: "center" })
 
-    const docContentElement  = props.document.querySelector('.layout-tab-container > div:not(.fn__none) >.protyle-content') as HTMLElement;
-    let doc_rect=docContentElement.getBoundingClientRect()
-    let mid_y=doc_rect.top+doc_rect.height/2
+    const docContentElement = props.document.querySelector('.layout-tab-container > div:not(.fn__none) > .protyle-content') as HTMLElement;
+    let doc_rect = docContentElement.getBoundingClientRect();
+    let mid_y = doc_rect.top + doc_rect.height / 2;
     let range_rect = range.getBoundingClientRect();
-    docContentElement.scrollTo({
-    top: range_rect.y - mid_y,
-    behavior: 'smooth'
-    })
+    range.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
     CSS.highlights.set("search-focus", new Highlight(range))
 }
